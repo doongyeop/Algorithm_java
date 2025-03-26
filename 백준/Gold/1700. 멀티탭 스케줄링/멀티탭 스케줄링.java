00968971472aea8a79355da86f6d8434f19bdb55
yeop <leedy977@gmail.com>
2025-03-27 01:57:29 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 100 ms, Memory: 13184 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -16,10 +16,11 @@ public class Main {
         int cnt = 0;
 
         for (int i = 0; i < k; i++) {
+            // 지금 꽂을 기기
             int electronic = electronics[i];
-
+            // 이미 꽂혀 있으면
             if (set.contains(electronic)) continue;
-
+            // 빈자리가 있을 때
             if (set.size() < n) {
                 set.add(electronic);
                 continue;
@@ -29,24 +30,30 @@ public class Main {
             int farthest = -1;
 
             for (int s : set) {
-                int next = Integer.MAX_VALUE;
-
+                // 꽂혀있는 기기가 다음에 사용할 순서
+                int next = k;
+                // 안꽂히면 k
                 for (int j = i + 1; j < k; j++) {
                     if (s == electronics[j]) {
                         next = j;
                         break;
                     }
-                }
-
+                } // for j
+                /*
+                2 3 2 3, 1 2 7
+                set : { 2, 3 }
+                2일 때 next: 5, farthest: 5, toUnplug = 2
+                7일 때 next: 7, farthest: 7, toUnplug = 3
+                 */
                 if (next > farthest) {
                     farthest = next;
                     toUnplug = s;
                 }
-            }
+            } // for set
             set.remove(toUnplug);
             set.add(electronic);
             cnt++;
-        }
+        } // for i
 
         System.out.println(cnt);
     }
