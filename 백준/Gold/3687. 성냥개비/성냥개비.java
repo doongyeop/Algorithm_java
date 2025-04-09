62aab46302712b2fec41f6da7e5bdbfbc35ab07c
yeop <leedy977@gmail.com>
2025-04-10 03:57:30 +0900

    [Gold II] Title: 성냥개비, Time: 100 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.java
+++ 백준/Gold/3687. 성냥개비/성냥개비.java
@@ -7,10 +7,26 @@ public class Main {
         int t = sc.nextInt();
         StringBuilder sb = new StringBuilder();
 
-        String[] add = {"1", "7", "4", "2", "0", "8"};
         long[] dp = new long[101];
         Arrays.fill(dp, Long.MAX_VALUE);
 
+        /*
+        0: 6
+        1: 2
+        2: 5
+        3: 5
+        4: 4
+        5: 5
+        6: 6
+        7: 3
+        8: 7
+        9: 6
+        최솟값 -> dp
+        최댓값 -> 1 붙여서 자릿수 늘리는게 가장 큼
+        i) 홀수일 때, 711111...
+        ii) 짝수일 때, 111111...
+         */
+        // 성냥개비로 만들 수 있는 최솟값
         dp[2] = 1;
         dp[3] = 7;
         dp[4] = 4;
@@ -18,11 +34,14 @@ public class Main {
         dp[6] = 6;
         dp[7] = 8;
         dp[8] = 10;
+        // 2 3 4 5 6 7 개로 만들 수 있는 숫자 최솟값
+        String[] nums = {"", "", "1", "7", "4", "2", "0", "8"};
 
         for (int i = 9; i <= 100; i++) {
             for (int j = 2; j <= 7; j++) {
                 if (dp[i - j] == Long.MAX_VALUE) continue;
-                String str = String.valueOf(dp[i - j]) + add[j - 2];
+                // j개를 써서 만들 수 있는 숫자 조합
+                String str = String.valueOf(dp[i - j]) + nums[j];
                 long num = Long.parseLong(str);
                 dp[i] = Math.min(dp[i], num);
             }
